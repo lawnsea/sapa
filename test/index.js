a58f41ee85966781ecb3f0f1253b3c6854800e69
Lon Ingram <lawnsea@gmail.com>
2014-05-27 00:32:27 -0500

    Transform resources to Resource* records
    Browsers that implement the [Resource Timing API][] emit `resource` entries in
    the Performance Timeline. Translate these to `ResourceSendRequest`,
    `ResourceReceiveResponse`, `ResourceReceivedData`, and `ResourceFinish`.
    
    The resulting timeline is sorted by `startTime` and then by `endTime`,
    ascending.
    
    For cross-origin requests, some metrics (`responseStart`, e.g.), may not be
    available. In that case, emit only `ResourceSendRequest` and `ResourceFinish`.
    
    [Resource Timing API]: http://www.w3.org/TR/resource-timing/

--- test/index.js
+++ test/index.js
@@ -18,6 +18,59 @@ module.exports = {
     test.done();
   },
 
+  'sorts all records by startTime and then by endTime': function (test) {
+    var records = sapa([{
+          name: 'foo',
+          entryType: 'mark',
+          startTime: 0,
+          duration: 0
+        }, {
+          name: 'http://example.com/foo',
+          entryType: 'resource',
+          startTime: 1,
+          requestStart: 2,
+          responseStart: 3,
+          responseEnd: 4,
+          duration: 3,
+
+          secureConnectionStart: 0,
+          connectEnd: 1,
+          connectStart: 1,
+          domainLookupEnd: 1,
+          domainLookupStart: 1,
+          fetchStart: 1,
+          redirectEnd: 0,
+          redirectStart: 0,
+          initiatorType: 'script'
+        }, {
+          name: 'bar',
+          entryType: 'mark',
+          startTime: 1,
+          duration: 0
+        }, {
+          name: 'applesauce',
+          entryType: 'mark',
+          startTime: 2,
+          duration: 0
+        }, {
+          name: 'chutney',
+          entryType: 'measure',
+          startTime: 3,
+          duration: 2
+        }]);
+
+      test.strictEqual(records[0].data.message, 'mark: foo');
+      test.strictEqual(records[1].data.message, 'mark: bar');
+      test.strictEqual(records[2].type, 'ResourceSendRequest');
+      test.strictEqual(records[3].data.message, 'mark: applesauce');
+      test.strictEqual(records[4].type, 'ResourceReceiveResponse');
+      test.strictEqual(records[5].type, 'ResourceReceivedData');
+      test.strictEqual(records[6].data.message, 'measure: chutney');
+      test.strictEqual(records[7].type, 'ResourceFinish');
+
+      test.done();
+  },
+
   'makes marks and measures TimeStamps with endTime === startTime + duration':
     function (test) {
       test.deepEqual(sapa([{
@@ -70,5 +123,232 @@ module.exports = {
           }
         }]);
       test.done();
+    },
+
+  'given a resource entry': {
+    'emits ResourceSendRequest': function (test) {
+      var entry = {
+        name: 'http://example.com/foo',
+        entryType: 'resource',
+        startTime: 1,
+        requestStart: 2,
+        duration: 2,
+
+        secureConnectionStart: 0,
+        connectEnd: 1,
+        connectStart: 1,
+        domainLookupEnd: 1,
+        domainLookupStart: 1,
+        fetchStart: 1,
+        redirectEnd: 0,
+        redirectStart: 0,
+        responseStart: 2,
+        responseEnd: 3,
+        initiatorType: 'script'
+      };
+
+      var record = sapa([entry])[0];
+
+      test.strictEqual(record.type, 'ResourceSendRequest');
+
+      test.strictEqual(record.data.url, entry.name, 'data.url === name');
+
+      test.strictEqual(
+        record.startTime, entry.startTime, 'startTime === startTime'
+      );
+
+      test.strictEqual(
+        record.endTime, entry.requestStart, 'endTime === requestStart'
+      );
+
+      entry.requestStart = 0;
+      record = sapa([entry])[0];
+
+      test.strictEqual(
+        record.endTime,
+        entry.startTime,
+        'if requestStart < startTime, endTime === startTime'
+      );
+
+      test.done();
+    },
+
+    'emits ResourceReceiveResponse if responseStart >= startTime':
+      function (test) {
+        var entry = {
+          name: 'http://example.com/foo',
+          entryType: 'resource',
+          responseStart: 2,
+
+          startTime: 1,
+          requestStart: 2,
+          duration: 2,
+          secureConnectionStart: 0,
+          connectEnd: 1,
+          connectStart: 1,
+          domainLookupEnd: 1,
+          domainLookupStart: 1,
+          fetchStart: 1,
+          redirectEnd: 0,
+          redirectStart: 0,
+          responseEnd: 3,
+          initiatorType: 'script'
+        };
+
+        var record = sapa([entry])[1];
+
+        test.strictEqual(record.type, 'ResourceReceiveResponse');
+
+        test.strictEqual(record.data.url, entry.name, 'data.url === name');
+
+        test.strictEqual(
+          record.startTime, entry.responseStart, 'startTime === responseStart'
+        );
+
+        test.strictEqual(
+          record.endTime, entry.responseStart, 'endTime === responseStart'
+        );
+
+        test.done();
+      },
+
+    'does not emit ResourceReceiveResponse if responseStart < startTime':
+      function (test) {
+        var entry = {
+          name: 'http://example.com/foo',
+          entryType: 'resource',
+          responseStart: 0,
+
+          startTime: 1,
+          requestStart: 2,
+          duration: 2,
+          secureConnectionStart: 0,
+          connectEnd: 1,
+          connectStart: 1,
+          domainLookupEnd: 1,
+          domainLookupStart: 1,
+          fetchStart: 1,
+          redirectEnd: 0,
+          redirectStart: 0,
+          responseEnd: 3,
+          initiatorType: 'script'
+        };
+
+        var records = sapa([entry]);
+
+        test.strictEqual(records.filter(function (record) {
+          return record.type === 'ResourceReceiveResponse';
+        }).length, 0, 'no ResourceReceiveResponse record emitted');
+
+        test.done();
+      },
+
+    'emits ResourceReceivedData if responseStart >= startTime':
+      function (test) {
+        var entry = {
+          name: 'http://example.com/foo',
+          entryType: 'resource',
+          responseStart: 2,
+          responseEnd: 3,
+
+          startTime: 1,
+          requestStart: 2,
+          duration: 2,
+          secureConnectionStart: 0,
+          connectEnd: 1,
+          connectStart: 1,
+          domainLookupEnd: 1,
+          domainLookupStart: 1,
+          fetchStart: 1,
+          redirectEnd: 0,
+          redirectStart: 0,
+          initiatorType: 'script'
+        };
+
+        var record = sapa([entry])[2];
+
+        test.strictEqual(record.type, 'ResourceReceivedData');
+
+        test.strictEqual(record.data.url, entry.name, 'data.url === name');
+
+        test.strictEqual(
+          record.startTime, entry.responseStart, 'startTime === responseStart'
+        );
+
+        test.strictEqual(
+          record.endTime, entry.responseEnd, 'endTime === responseEnd'
+        );
+
+        test.done();
+      },
+
+    'does not emit ResourceReceivedData if responseStart < startTime':
+      function (test) {
+        var entry = {
+          name: 'http://example.com/foo',
+          entryType: 'resource',
+          responseStart: 0,
+          responseEnd: 3,
+
+          startTime: 1,
+          requestStart: 2,
+          duration: 2,
+          secureConnectionStart: 0,
+          connectEnd: 1,
+          connectStart: 1,
+          domainLookupEnd: 1,
+          domainLookupStart: 1,
+          fetchStart: 1,
+          redirectEnd: 0,
+          redirectStart: 0,
+          initiatorType: 'script'
+        };
+
+        var records = sapa([entry]);
+
+        test.strictEqual(records.filter(function (record) {
+          return record.type === 'ResourceReceivedData';
+        }).length, 0, 'no ResourceReceivedData record emitted');
+
+        test.done();
+      },
+
+    'emits ResourceFinish': function (test) {
+      var entry = {
+        name: 'http://example.com/foo',
+        entryType: 'resource',
+        responseEnd: 3,
+
+        startTime: 1,
+        requestStart: 2,
+        duration: 2,
+        secureConnectionStart: 0,
+        connectEnd: 1,
+        connectStart: 1,
+        domainLookupEnd: 1,
+        domainLookupStart: 1,
+        fetchStart: 1,
+        redirectEnd: 0,
+        redirectStart: 0,
+        responseStart: 2,
+        initiatorType: 'script'
+      };
+
+      var record = sapa([entry])[3];
+
+      test.strictEqual(record.type, 'ResourceFinish');
+
+      test.strictEqual(record.data.url, entry.name, 'data.url === name');
+
+      test.strictEqual(
+        record.startTime, entry.responseEnd, 'startTime === responseEnd'
+      );
+
+      test.strictEqual(
+        record.endTime, entry.responseEnd, 'endTime === responseEnd'
+      );
+
+      test.done();
     }
+  }
 };
